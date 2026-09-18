94e9f467ac6872ca071f12e679d8b77b87467a57
TailsX2 <88992905+TailsX2@users.noreply.github.com>
2026-09-18 22:18:14 +0700

    pertemuan 3

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -29,4 +29,12 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+    public function tickets()
+    {
+        return $this->hasMany(Ticket::class);
+    }
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
